a83b075a67ba71cb38f8f2526b7fd4825453a620
Ryan <lbrnryn@yahoo.com>
2022-06-08 18:13:54 +0800

    Remove dev code

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 async function main() {
-  await mongoose.connect(process.env.NODE_ENV == 'development' ? 'mongodb://localhost:27017/blog': process.env.MONGO_URI);
+  // await mongoose.connect(process.env.NODE_ENV == 'development' ? 'mongodb://localhost:27017/blog': process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI);
   console.log('Database connected!');
 }
 main()
